9a868bcccf4ba3606508fbd420bd5ab3125a61fd
AlphaGamer2k20 <84835168+AlphaGamer2k20@users.noreply.github.com>
2021-06-28 19:43:04 +0530

    Add files via upload

--- Lets-Chat-Web-App-Room.js
+++ Lets-Chat-Web-App-Room.js
@@ -53,5 +53,5 @@ function redirectToRoomName(name)
 function logout() {
  localStorage.removeItem("user_name");
  localStorage.removeItem("room_name");
-     window.location = "index.html";
+     window.location = "Lets-Chat-Web-App.html";
 }
